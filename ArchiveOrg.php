1b84a00d794c6ae9f57aaacb14b3afe3fc662a83
Patrick Savalle <patrick@patricksavalle.com>
2021-10-24 14:42:15 +0200

    Added Archive.org functionality

---
+++ ArchiveOrg.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ContentSyndication {
+
+    class ArchiveOrg
+    {
+        static public function check(string $url): bool
+        {
+            assert(filter_var($url, FILTER_VALIDATE_URL) !== false);
+            // HEAD request
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_NOBODY, true); // set to HEAD request
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // don't output the response
+            curl_exec($ch);
+            $result = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+            return $result >= 200 and $result < 400;
+        }
+
+        static public function closest(string $url) /*: string*/
+        {
+            assert(filter_var($url, FILTER_VALIDATE_URL) !== false);
+            $tmp = "https://archive.org/wayback/available?url=$url";
+            $json = json_decode((new HttpRequest)($tmp));
+            return $json->archived_snapshots->closest->url ?? false;
+        }
+
+        static public function archive(string $url): string
+        {
+            assert(filter_var($url, FILTER_VALIDATE_URL) !== false);
+            // store in webarchive.com and get newly archived url back
+            $url = "https://web.archive.org/save/$url";
+            (new HttpRequest)($url);
+            return $url;
+        }
+
+        static public function archiveAsync(string $url)
+        {
+            assert(filter_var($url, FILTER_VALIDATE_URL) !== false);
+            // store in webarchive.com and get newly archived url back
+            (new HttpFireAndForgetRequest)("https://web.archive.org/save/$url");
+        }
+    }
+}
--- HttpRequest.php
+++ HttpRequest.php
@@ -18,16 +18,13 @@ namespace ContentSyndication {
             if (!empty($user) or !empty($pass)) {
                 curl_setopt($curl, CURLOPT_USERPWD, "$user:$pass");
             }
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // follow redirects
-            curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // set referer on redirect
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // follow redirects
+            curl_setopt($curl, CURLOPT_AUTOREFERER, true); // set referer on redirect
             curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 OPR/78.0.4093.112"); // some feeds require a user agent
             curl_setopt($curl, CURLOPT_HEADER, false);
             curl_setopt($curl, CURLOPT_TIMEOUT, 20);
             curl_setopt($curl, CURLOPT_ENCODING, '');
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // no echo, just return result
-            if (!ini_get('open_basedir')) {
-                curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // sometime is useful :)
-            }
             $result = curl_exec($curl);
             if ($result === false or curl_getinfo($curl, CURLINFO_HTTP_CODE) !== 200) {
                 throw new Exception("CURL exception on: " . $url);
